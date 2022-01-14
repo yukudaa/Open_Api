e0ae3f60e372d9b732a58c55b869fb0812369dfc
yukuda <leeyookoo@naver.com>
2022-01-15 00:42:02 +0900

    OpenApiApplicationTests

---
+++ plantOpenAPI-master/src/test/java/start/openApi/OpenApiApplicationTests.java
@@ -0,0 +1,14 @@
+package start.openApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
