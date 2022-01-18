6f760ed65725efb34cf33fe80160fa11319a1e3d
yukuda <leeyookoo@naver.com>
2022-01-18 16:18:11 +0900

    DbRepositoryTest

---
+++ plantOpenAPI-master/src/test/java/start/openApi/openapi/xml/DbRepositoryTest.java
@@ -0,0 +1,24 @@
+//package start.openApi.openapi.xml;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//class DbRepositoryTest {
+//
+//    @Autowired DbRepository dbRepository;
+//
+//    @Test
+//    public void testDB() throws Exception {
+//        DB db = new DB();
+//        db.setCategory("yyy");
+//
+//    }
+//
+//}
