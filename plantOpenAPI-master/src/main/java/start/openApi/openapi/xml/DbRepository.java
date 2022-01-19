2d18b4f36804dd3f7d54baddd31980d30fd37b6f
yukuda <leeyookoo@naver.com>
2022-01-19 16:03:28 +0900

    Dbrepository

--- plantOpenAPI-master/src/main/java/start/openApi/openapi/xml/DbRepository.java
+++ plantOpenAPI-master/src/main/java/start/openApi/openapi/xml/DbRepository.java
@@ -6,20 +6,8 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@Repository
-public class DbRepository {
 
-    @PersistenceContext
-    private EntityManager em;
-
-    public Long save(DB db) {
-        em.persist(db);
-        return db.getId();
-    }
-
-    public DB find(int id) {
-        return em.find(DB.class,id);
-    }
+public interface DbRepository extends JpaRepository<DB,Long> {
 
 
 }
